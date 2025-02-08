93945f5ab91c65e201dfa99a3d609114d725e1a7
= <ygor-skat@hotmail.com>
2025-02-08 13:00:02 -0300

    Create duckdb database and handles data with duckdb in get_all_questions and delete

--- api/app.py
+++ api/app.py
@@ -1,22 +1,25 @@
 from flask import Flask, jsonify, request
+import duckdb
 import db_utils
 
-app = Flask(__name__)
+DUCKDB_URI = 'database_duckdb.db' # The path of duckdb database
 
+app = Flask(__name__)
+con = duckdb.connect(DUCKDB_URI)
 
 @app.route('/questions/all', methods=['GET'])
 def get_all_questions():
-    return jsonify(db_utils.retrieve_database())
+    return jsonify(db_utils.retrieve_database_duckdb(con))
 
 @app.route('/questions/new', methods=['POST'])
 def add_question():
     question_data = request.get_json(force=True)
-    db_utils.write_line_to_database(question_data)
+    db_utils.write_line_to_database_duckdb(con, question_data)
     return question_data
 
 @app.route('/questions/del/<question_id>', methods=['POST'])
 def del_question(question_id):
-    db_utils.delete_question_database(int(question_id))
+    db_utils.delete_question_database_duckdb(con, question_id)
     return f'Question {question_id} was deleted. If it doesn\'t exist, nothing was done!'
 
 if __name__ == '__main__':
--- api/db_utils.py
+++ api/db_utils.py
@@ -1,6 +1,65 @@
 import ast
+from duckdb import DuckDBPyConnection
 
-DATABASE_URI = 'data.db' # The path of database file
+# Db utils for duckdb
+
+
+
+def retrieve_database_duckdb(con: DuckDBPyConnection) -> str:
+    '''
+    Retrieves all data in the database.
+
+    '''
+
+    query = '''
+        SELECT * FROM jlpt_questions
+
+    '''
+
+    con.execute(query)
+    
+    return con.fetchall()
+
+def write_line_to_database_duckdb(con: DuckDBPyConnection, data: dict) -> None:
+    '''
+    Inserts new data to the duckdb database.
+    '''
+
+    question_id_data, question_str_data, question_choices_data, question_answer_data = data.values()
+
+    query = f'''
+
+        INSERT INTO jlpt_questions (question_id, question_str, question_choices, question_answer)
+        VALUES
+        (
+            {question_id_data},
+            {question_str_data},
+            {question_choices_data},
+            {question_answer_data}
+        )
+    '''
+
+    con.execute(query)
+    
+
+
+def delete_question_database_duckdb(con: DuckDBPyConnection, question_id_data: int) -> None:
+    '''
+    Delete a specific line/question from duckdb database.
+    '''
+    question_id_data = str(question_id_data)
+
+    query = f'''
+
+        DELETE FROM jlpt_questions WHERE question_id = {question_id_data}
+
+    '''
+
+    con.execute(query)
+
+
+# Db utils for file db
+DATABASE_FILE_URI = 'database_file.db' # The path of database file
 
 def retrieve_database() -> str:
     '''
@@ -10,7 +69,7 @@ def retrieve_database() -> str:
 
     data = list()
 
-    with open(DATABASE_URI, 'r') as f:
+    with open(DATABASE_FILE_URI, 'r') as f:
         for line in f:
             data.append(ast.literal_eval(line.strip()))
     
@@ -21,7 +80,7 @@ def write_line_to_database(data: dict) -> None:
     Append new line to the end of the database file.
     '''
 
-    with open(DATABASE_URI, 'a') as f:
+    with open(DATABASE_FILE_URI, 'a') as f:
         f.write('\n')
         f.write(str(data))
 
@@ -32,7 +91,7 @@ def write_to_database(data: list) -> None:
     IMPORTANT: this function erases all previous data.
     '''
 
-    with open(DATABASE_URI, 'w+') as f:
+    with open(DATABASE_FILE_URI, 'w+') as f:
         for index, line in enumerate(data):
             f.write(str(line))
             if index != len(data) - 1:
@@ -43,6 +102,7 @@ def delete_question_database(question_id: int) -> None:
     '''
     Delete a specific line/question from database file.
     '''
+    question_id = str(question_id)
 
     data = retrieve_database()
     for question in data:
